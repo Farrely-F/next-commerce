43884c7c5d64d4229d6f05346e15eb68d820af7f
farrely-f <farrely.firenza@outlook.com>
2023-12-25 01:49:11 +0700

    feat: setup login function and page ui

--- src/pages/auth/login.tsx
+++ src/pages/auth/login.tsx
@@ -1,7 +1,9 @@
+import LoginView from "@/components/views/auth/Login";
+
 export default function LoginPage() {
   return (
     <main>
-      <h1>Login</h1>
+      <LoginView />
     </main>
   );
 }

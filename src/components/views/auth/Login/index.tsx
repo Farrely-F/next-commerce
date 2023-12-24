5415d7caf2e7e4feeebb3e85ffe515c16f7c8507
farrely-f <farrely.firenza@outlook.com>
2023-12-25 02:50:06 +0700

    feat: firebase-setup-login-with-google

--- src/components/views/auth/Login/index.tsx
+++ src/components/views/auth/Login/index.tsx
@@ -7,6 +7,7 @@ export default function LoginView() {
   const router = useRouter();
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState("");
+  const [isRevealed, setIsRevealed] = useState(false);
 
   const callbackUrl: string = (router.query.callbackUrl as string) || "/";
 
@@ -44,11 +45,11 @@ export default function LoginView() {
       <div className="mx-auto max-w-screen-xl px-4 py-16 sm:px-6 lg:px-8">
         <div className="mx-auto max-w-lg">
           <h1 className="text-center text-2xl font-bold text-indigo-600 sm:text-3xl">
-            Register Now
+            Sign In Now
           </h1>
 
           <p className="mx-auto mt-4 max-w-md text-center text-gray-500">
-            Please Register to use Our Service
+            Please Login First to use Our Service
           </p>
 
           <form
@@ -94,13 +95,17 @@ export default function LoginView() {
 
               <div className="relative">
                 <input
-                  type="password"
+                  type={isRevealed ? "text" : "password"}
                   name="password"
                   className="w-full rounded-lg border-gray-200 p-4 pe-12 text-sm shadow-sm"
                   placeholder="Enter password"
                 />
 
-                <span className="absolute inset-y-0 end-0 grid place-content-center px-4">
+                <button
+                  type="button"
+                  onClick={() => setIsRevealed(!isRevealed)}
+                  className="absolute inset-y-0 end-0 grid place-content-center px-4"
+                >
                   <svg
                     xmlns="http://www.w3.org/2000/svg"
                     className="h-4 w-4 text-gray-400"
@@ -121,7 +126,7 @@ export default function LoginView() {
                       d="M2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542-7z"
                     />
                   </svg>
-                </span>
+                </button>
               </div>
             </div>
 
@@ -132,7 +137,46 @@ export default function LoginView() {
             >
               {isLoading ? "Signing In" : "Sign In"}
             </button>
-
+            <div className="border-t border-b border-slate-200"></div>
+            <div>
+              <button
+                className="border border-indigo-600 text-indigo-600 w-full rounded-lg px-5 py-3 text-sm font-medium flex justify-center items-center gap-4"
+                type="button"
+                onClick={() =>
+                  signIn("google", { callbackUrl, redirect: false })
+                }
+              >
+                <span>
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="24"
+                    height="24"
+                    viewBox="0 0 24 24"
+                  >
+                    <path
+                      fill="#4f46e5"
+                      d="M12.222 5.977a5.402 5.402 0 0 1 3.823 1.496l2.868-2.868A9.61 9.61 0 0 0 12.222 2a9.996 9.996 0 0 0-8.937 5.51l3.341 2.59a5.96 5.96 0 0 1 5.596-4.123"
+                      opacity=".7"
+                    />
+                    <path
+                      fill="#4f46e5"
+                      d="M3.285 7.51a10.013 10.013 0 0 0 0 8.98l3.341-2.59a5.913 5.913 0 0 1 0-3.8z"
+                    />
+                    <path
+                      fill="#4f46e5"
+                      d="M15.608 17.068A6.033 6.033 0 0 1 6.626 13.9l-3.34 2.59A9.996 9.996 0 0 0 12.221 22a9.547 9.547 0 0 0 6.618-2.423z"
+                      opacity=".5"
+                    />
+                    <path
+                      fill="#4f46e5"
+                      d="M21.64 10.182h-9.418v3.868h5.382a4.6 4.6 0 0 1-1.996 3.018l-.01.006l.01-.006l3.232 2.51a9.753 9.753 0 0 0 2.982-7.35c0-.687-.06-1.371-.182-2.046"
+                      opacity=".25"
+                    />
+                  </svg>
+                </span>
+                Sign In With Google
+              </button>
+            </div>
             <p className="text-center text-sm text-gray-500">
               Dont Have Account?
               <Link href={"/auth/register"} className="underline ml-2">
